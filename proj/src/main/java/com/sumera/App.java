e67ad08ec3cd3eb7d60dca8fba1aa0312dcf8ee6
Sumera Shaik <sumera@localhost.localdomain>
2014-11-25 10:45:37 +0530

    adding new project

---
+++ proj/src/main/java/com/sumera/App.java
@@ -0,0 +1,13 @@
+package com.sumera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ proj/src/main/java/com/sumera/example.java
@@ -0,0 +1,8 @@
+package com.sumera;
+public class example
+{
+ public static void main(String args[])
+ {
+  System.out.println("this is an example");
+ }
+ }
